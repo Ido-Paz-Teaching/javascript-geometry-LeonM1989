d204f7efadf796a0681a1aa6f5ae818c0419ba4b
Leon M <leon5916619@gmail.com>
2021-11-17 01:28:38 +0200

    all.test.passed

--- geometry.js
+++ geometry.js
@@ -1,13 +1,17 @@
 function getTriangleArea(height, width) {
   /* Your amazing code here 🚀 */
+  return Number(width * height / 2);
 }
 
 function getSquareArea(side) {
   /* Your amazing code here 🚀 */
+  return Number(Math.pow(side, 2));
 }
 
 function getCircleArea(radius) {
+  
   /* Your amazing code here 🚀 */
+  return Number((Math.PI*(Math.pow(radius, 2))).toFixed(2));
 }
 
 //Please , don't remove the following code 
